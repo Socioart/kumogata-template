4286560abb5df7dd6bb2aaffddc525f8dc779b3a
labocho <labocho@penguinlab.jp>
2020-11-26 16:58:30 +0900

    classify Stack

--- Rakefile
+++ Rakefile
@@ -1,5 +1,6 @@
 require "yaml"
 require "io/console"
+require "shellwords"
 require_relative "lib/functions"
 
 def ask_yesno(prompt)
@@ -8,47 +9,55 @@ def ask_yesno(prompt)
 end
 
 def kumogata2(stack, subcommand_and_args)
-  sh "kumogata2 --profile #{stack["profile"]} --region #{stack["region"]} --capabilities CAPABILITY_IAM #{subcommand_and_args}"
+  sh "kumogata2 --profile #{stack.profile} --region #{stack.region} --capabilities CAPABILITY_IAM,CAPABILITY_NAMED_IAM #{subcommand_and_args}"
 end
 
-STACKS = Dir["stacks/*.yml"].map {|f| YAML.load_file(f) }
+
+STACKS = Dir["stacks/*.yml"].map {|f| Stack.load_file(f) }
+DASHBOARDS = Dir["dashboards/*.yml"].map {|f| File.basename(f).gsub(/\.yml$/, "") }
+
 STACKS.each do |stack|
-  namespace stack["name"] do
+  namespace stack.name do
     namespace "state" do
-      stack["states"].keys.each do |state|
-        desc "Set #{stack["name"]} state state to #{state}"
+      stack.states.keys.each do |state|
+        desc "Set #{stack.name} state state to #{state}"
         task state do
-          mkdir_p "states"
-          File.write("states/#{stack["name"]}", state)
+          mkdir_p "stacks"
+          File.write("stacks/#{stack.name}.state", state)
         end
       end
     end
 
+    task "state" do
+      mkdir_p "stacks"
+      puts File.read("stacks/#{stack.name}.state")
+    end
+
     task "prepare" do
-      add_yaml_tags(stack)
+      Stack.current = stack
     end
 
     task "build" => "prepare" do
-      template = build_template
+      template = build_template(stack)
       mkdir_p "tmp"
-      File.write("tmp/#{stack["name"]}.yml", template)
+      File.write("tmp/#{stack.name}.yml", template)
     end
 
-    desc "Show template for #{stack["name"]}"
+    desc "Show template for #{stack.name}"
     task "show" => "build" do
-      sh "cat tmp/#{stack["name"]}.yml"
+      sh "cat tmp/#{stack.name}.yml"
     end
 
-    desc "Create stack #{stack["name"]}"
+    desc "Create stack #{stack.name}"
     task "create" => "build" do
-      kumogata2 stack, "create tmp/#{stack["name"]}.yml #{stack["name"]}"
+      kumogata2 stack, "create tmp/#{stack.name}.yml #{stack.name}"
     end
 
-    desc "Update stack #{stack["name"]}"
+    desc "Update stack #{stack.name}"
     task "update" => "build" do
-      kumogata2 stack, "dry-run tmp/#{stack["name"]}.yml #{stack["name"]}"
+      kumogata2 stack, "dry-run tmp/#{stack.name}.yml #{stack.name}"
       if ask_yesno("apply? (y/n): ")
-        kumogata2 stack, "update tmp/#{stack["name"]}.yml #{stack["name"]}"
+        kumogata2 stack, "update tmp/#{stack.name}.yml #{stack.name}"
       end
     end
   end
--- lib/functions.rb
+++ lib/functions.rb
@@ -2,14 +2,72 @@ require "yaml"
 require "json"
 require "active_support/core_ext/hash"
 
-def add_yaml_tags(stack)
-  unless File.exist?("states/#{stack["name"]}")
-    raise "Cannot found state for #{stack["name"]}. Please run rake #{stack["name"]}:state:*"
+class Stack
+  attr_reader :name, :profile, :region, :states, :resources
+
+  class << self
+    attr_accessor :current
+
+    def load_file(path)
+      new(YAML.load_file(path))
+    end
   end
 
-  state_name = File.read("states/#{stack["name"]}").strip
-  variables = stack.fetch("variables").deep_merge(stack.fetch("states").fetch(state_name))
+  def initialize(attributes)
+    @state_loaded = false
 
+    %w(name profile region resources).each do |key|
+      instance_variable_set("@#{key}", attributes[key])
+    end
+
+    @default_variables = attributes.fetch("variables")
+    @states = load_states
+  end
+
+  def variables
+    @variables ||= @default_variables.deep_merge(states.fetch(state_name))
+  end
+
+  def var(dot_splitted_name)
+    if state_loaded?
+      variables.dig(*dot_splitted_name.split("."))
+    else
+      @default_variables.dig(*dot_splitted_name.split("."))
+    end
+  end
+
+  def state_name
+    @state_name ||= begin
+      unless File.exist?("stacks/#{name}.state")
+        raise "Cannot found state for #{name}. Please run rake #{name}:state:*"
+      end
+
+      File.read("stacks/#{name}.state").strip
+    end
+  end
+
+  private
+  def state_loaded?
+    @state_loaded
+  end
+
+  def load_states
+    current = self.class.current
+    self.class.current = self
+
+    states = Dir["states/*.yml"].each_with_object({}) do |f, h|
+      File.basename(f) =~ /(.*).yml/
+      h[$1] = YAML.load_file(f)
+    end
+
+    @state_loaded = true
+    states
+  ensure
+    self.class.current = current
+  end
+end
+
+def add_yaml_tags
   # Register tags to parse abbreviated function call like `!Ref: Foo`
   # See: https://docs.aws.amazon.com/ja_jp/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference.html
   %w(And Base64 Cidr Equals FindInMap GetAZs If ImportValue Join Not Or Select Split Sub Transform).each do |tag|
@@ -22,26 +80,34 @@ def add_yaml_tags(stack)
     {"Fn::GetAtt" => value.split(".")}
   end
 
+  YAML.add_domain_type("", "Join") do |_type, value|
+    {"Fn::Join" => value}
+  end
+
   YAML.add_domain_type("", "Ref") do |_type, value|
     {"Ref" => value}
   end
 
   YAML.add_domain_type("", "Var") do |_type, value|
-    var = variables.dig(*value.split("."))
+    var = Stack.current.var(value)
     raise "Unknown variable `#{value}`" if var.nil?
 
     var
   end
 end
 
-def build_template
+add_yaml_tags
+
+def build_template(stack)
   template = {
     "AWSTemplateFormatVersion" => "2010-09-09",
     "Resources" => {},
   }
 
-  Dir.glob("resources/**/*.yml").sort.each do |yml|
-    YAML.load_file(yml).each do |k, v|
+  (stack.resources || Dir.glob("resources/**/*.yml")).sort.each do |yml|
+    next unless (resources = YAML.load_file(yml))
+
+    resources.each do |k, v|
       template["Resources"][k] = v
     end
   end
